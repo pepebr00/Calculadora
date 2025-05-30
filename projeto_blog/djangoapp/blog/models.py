09886e6eb61b6126088ccb447fe47177c54e6b1a
Pedro <pedroduraesbueno@gmail.com>
2025-05-29 22:08:51 -0300

    Projeto Blog

--- projeto_blog/djangoapp/blog/models.py
+++ projeto_blog/djangoapp/blog/models.py
@@ -1,6 +1,27 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
+from django_summernote.models import AbstractAttachment
+from utils.images import resize_image
 from utils.rands import slugify_new
+
+
+class PostAttachment(AbstractAttachment):
+    def save(self, *args, **kwargs):
+        if not self.name:
+            self.name = self.file.name
+
+        current_file_name = str(self.file.name)
+        super_save = super().save(*args, **kwargs)
+        file_changed = False
+
+        if self.file:
+            file_changed = current_file_name != self.file.name
+
+        if file_changed:
+            resize_image(self.file, 900, True, 70)
+
+        return super_save
 
 
 class Tag(models.Model):
@@ -67,10 +88,19 @@ class Page(models.Model):
         return self.title
 
 
+class PostManager(models.Manager):
+    def get_published(self):
+        return self\
+            .filter(is_published=True)\
+            .order_by('-pk')
+
+
 class Post(models.Model):
     class Meta:
         verbose_name = 'Post'
         verbose_name_plural = 'Posts'
+
+    objects = PostManager()
 
     title = models.CharField(max_length=65,)
     slug = models.SlugField(
@@ -116,7 +146,23 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse('blog:index')
+        return reverse('blog:post', args=(self.slug,))
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.title, 4)
-        return super().save(*args, **kwargs)
+
+        current_cover_name = str(self.cover.name)
+        super_save = super().save(*args, **kwargs)
+        cover_changed = False
+
+        if self.cover:
+            cover_changed = current_cover_name != self.cover.name
+
+        if cover_changed:
+            resize_image(self.cover, 900, True, 70)
+
+        return super_save
